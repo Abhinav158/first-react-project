6eae7ad3ed5212df23a55bfa2dddc35c359a9f53
Abhinav Raghunandan <93826081+Abhinav158@users.noreply.github.com>
2022-10-04 00:35:39 +0530

    Add files via upload

---
+++ components/Main.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function Main() {
+    return (
+        <main>
+            <h1 className="main--title">Fun facts about React</h1>
+            <ul className="main--facts">
+                <li>Released in the year 2013 by Jordan Walke</li>
+                <li>Has over 100K stars on GitHub</li>
+                <li>Has a very strong dev community</li>
+                <li>A Declarative and Composable framework</li>
+                <li>Virtual DOM runs a lot faster than Real DOM</li>
+                <li>Powers thousands of enterprise apps!</li>
+            </ul>
+        </main>
+    )
+}
